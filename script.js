a8fd11fe487a913d8e25a54e9fb022841ef5dda9
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-03-09 11:49:06 +0600

    login page interactivity done

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  //1- get the username input
+  const username = document.getElementById("username").value;
+  console.log(username);
+
+  //2- get the password
+  const password = document.getElementById("password").value;
+  console.log(password);
+
+  //3- match username & password
+  if (username === "admin" && password === "admin123") {
+    //3-1 true:::>> alert> homepage
+    alert("Sign in Successfully");
+
+    // window.location.replace("/./index_app.html");
+    window.location.assign("./index_home.html");
+  } else {
+    //3-2 false:::>> alert> return
+    alert("Invalid Username or Password");
+    return;
+  }
+});
